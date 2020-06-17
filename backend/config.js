17c85956af234391b066ac740d574114d71505c0
Benjamin S Ogzewalla <saigeogzewalla@gmail.com>
2020-06-16 20:20:57 -0400

    Update

--- backend/config.js
+++ backend/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 export default {
   PORT: process.env.PORT || 5000,
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/amazini',
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://Benji:gumdrops@realmcluster-qserk.mongodb.net/RealmCluster?retryWrites=true',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
 };
